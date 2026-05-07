97e9fdf0d9c8ca148e1714b741dc29a79c180696
tex batista <andersonrexbatista@gmail.com>
2026-05-07 19:29:53 -0300

    feat: adiciona geração de relatórios html com mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: 'mochawesome',
+
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+
   e2e: {
     baseUrl: "https://saucedemo.com/",
 
